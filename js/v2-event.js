0f135b2af75a60ba1b8e95716a8b33f3b81d220c
jaesukpark77 <jaesukpark77@naver.com>
2022-02-24 17:54:48 +0900

    VueJs 수업
    v2-event.js 코드 추가

--- js/v2-event.js
+++ js/v2-event.js
@@ -71,3 +71,63 @@ const vm5 = new Vue({
         }
     }
 })
+
+const vm6 = new Vue({
+    el:'#vm6',
+    data:{
+        text:'마우스로 누르세요.'
+    },
+    methods: {
+        doThis: function(){
+            this.text = '마우스가 눌렸어요.';
+        },
+        doThat: function(){
+            this.text = '마우스에서 손울 땠어요.';
+        }
+    }
+});
+
+const vm7 = new Vue({
+    el:'#vm7',
+    data:{
+        doText:'마우스로 올려놓으세요.',
+        doBg:'#eee',
+        doColor:'#000'
+    },
+    methods: {
+        doOn: function(){
+            this.doText = '마우스가 올라갔어요.';
+            this.doBg = 'red';
+            this.doColor = '#fff';
+        },
+        doLeft: function(){
+            this.doText = '마우스가 벗어났어요.';
+            this.doBg = 'gold';
+            this.doColor = '#000';
+        }
+    }
+});
+
+const vm8 = new Vue({
+    el:'#vm8',
+    data:{
+        doTab:'Tab키를 누르세요.',
+    },
+    methods: {
+        doFocus: function(){
+            this.doTab = 'Focus 되었습니다.';
+        },
+        doBlur: function(){
+            this.doTab = 'Blur 되었습니다.';
+        }
+    }
+});
+
+const vm9 = new Vue({
+    el:'#vm9',
+    methods: {
+        enterAlert: function(){
+            alert('Shift와 Enter키를 눌렀습니다.');
+        }
+    }
+});
